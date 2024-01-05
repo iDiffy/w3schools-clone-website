b6e5258deedb07620d363e3606b998b1cbe61ea3
iDiffy <126216900+iDiffy@users.noreply.github.com>
2024-01-05 15:48:08 +0800

    w3schools clone website
    html css js

---
+++ Data/ExercisesandQuizzes.js
@@ -0,0 +1,407 @@
+export const ExercisesandQuizzes = {
+    name : "Exercises and Quizzes",
+    Exercises : {
+        name : "Exercises",
+        menu : [
+            {
+                title : "HTML Exercises",
+                link : "https://www.w3schools.com/html/html_exercises.asp"
+            },
+            {
+                title : "CSS Exercises",
+                link : "https://www.w3schools.com/css/css_exercises.asp"
+            },
+            {
+                title : "JavaScript Exercises",
+                link : "https://www.w3schools.com/js/js_exercises.asp"
+            },
+            {
+                title : "Python Exercises",
+                link : "https://www.w3schools.com/python/python_exercises.asp"
+            },
+            {
+                title : "SQL Exercises",
+                link : "https://www.w3schools.com/sql/sql_exercises.asp"
+            },
+            {
+                title : "PHP Exercises",
+                link : "https://www.w3schools.com/php/php_exercises.asp"
+            },
+            {
+                title : "Java Exercises",
+                link : "https://www.w3schools.com/java/java_exercises.asp"
+            },
+            {
+                title : "C Exercises",
+                link : "https://www.w3schools.com/c/c_exercises.php"
+            },
+            {
+                title : "C++ Exercises",
+                link : "https://www.w3schools.com/cpp/cpp_exercises.asp"
+            },
+            {
+                title : "C# Exercises",
+                link : "https://www.w3schools.com/cs/cs_exercises.php"
+            },
+            {
+                title : "jQuery Exercises",
+                link : "https://www.w3schools.com/jquery/jquery_exercises.asp"
+            },
+            {
+                title : "React.js Exercises",
+                link : "https://www.w3schools.com/react/react_exercises.asp"
+            },
+            {
+                title : "MySQL Exercises",
+                link : "https://www.w3schools.com/mysql/mysql_exercises.asp"
+            },
+            {
+                title : "Bootstrap 5 Exercises",
+                link : "https://www.w3schools.com/bootstrap5/index.php"
+            },
+            {
+                title : "Bootstrap 4 Exercises",
+                link : "https://www.w3schools.com/bootstrap4/default.asp"
+            },
+            {
+                title : "Bootstrap 3 Exercises",
+                link : "https://www.w3schools.com/bootstrap/bootstrap_exercises.asp"
+            },
+            {
+                title : "NumPy Exercises",
+                link : "https://www.w3schools.com/python/numpy/numpy_exercises.asp"
+            },
+            {
+                title : "Pandas Exercises",
+                link : "https://www.w3schools.com/python/pandas/pandas_exercises.asp"
+            },
+            {
+                title : "SciPy Exercises",
+                link : "https://www.w3schools.com/python/scipy/scipy_exercises.php"
+            },
+            {
+                title : "TypeScript Exercises",
+                link : "https://www.w3schools.com/typescript/typescript_exercises.php"
+            },
+            {
+                title : "Excel Exercises",
+                link : "https://w3schools.com/excel/excel_exercises.php"
+            },
+            {
+                title : "R Exercises",
+                link : "https://www.w3schools.com/r/r_exercises.asp"
+            },
+            {
+                title : "Git Exercises",
+                link : "https://www.w3schools.com/git/git_exercises.asp"
+            },
+            {
+                title : "Kotlin Exercises",
+                link : "https://www.w3schools.com/kotlin/kotlin_exercises.php"
+            },
+            {
+                title : "Go Exercises",
+                link : "https://www.w3schools.com/go/go_exercises.php"
+            },
+            {
+                title : "MongoDB Exercises",
+                link : "https://www.w3schools.com/mongodb/mongodb_exercises.php"
+            },
+        ]
+    },
+    Quizzes : {
+        name : "Quizzes",
+        menu : [
+            {
+                title : "HTML Quiz",
+                link : "https://www.w3schools.com/html/html_quiz.asp"
+            },
+            {
+                title : "CSS Quiz",
+                link : "https://www.w3schools.com/css/css_quiz.asp"
+            },
+            {
+                title : "JavaScript Quiz",
+                link : "https://www.w3schools.com/js/js_quiz.asp"
+            },
+            {
+                title : "Python Quiz",
+                link : "https://www.w3schools.com/python/python_quiz.asp"
+            },
+            {
+                title : "SQL Quiz",
+                link : "https://www.w3schools.com/sql/sql_quiz.asp"
+            },
+            {
+                title : "PHP Quiz",
+                link : "https://www.w3schools.com/php/php_quiz.asp"
+            },
+            {
+                title : "Java Quiz",
+                link : "https://www.w3schools.com/java/java_quiz.asp"
+            },
+            {
+                title : "C Quiz",
+                link : "https://www.w3schools.com/c/c_quiz.php"
+            },
+            {
+                title : "C++ Quiz",
+                link : "https://www.w3schools.com/cpp/cpp_quiz.asp"
+            },
+            {
+                title : "C# Quiz",
+                link : "https://www.w3schools.com/cs/cs_quiz.php"
+            },
+            {
+                title : "jQuery Quiz",
+                link : "https://www.w3schools.com/jquery/jquery_quiz.asp"
+            },
+            {
+                title : "React.js Quiz",
+                link : "https://www.w3schools.com/react/react_quiz.asp"
+            },
+            {
+                title : "MySQL Quiz",
+                link : "https://www.w3schools.com/mysql/mysql_quiz.asp"
+            },
+            {
+                title : "Bootstrap 5 Quiz",
+                link : "https://www.w3schools.com/bootstrap5/bootstrap_quiz.php"
+            },
+            {
+                title : "Bootstrap 4 Quiz",
+                link : "https://www.w3schools.com/bootstrap4/bootstrap_quiz.asp"
+            },
+            {
+                title : "Bootstrap 3 Quiz",
+                link : "https://www.w3schools.com/bootstrap/bootstrap_quiz.asp"
+            },
+            {
+                title : "NumPy Quiz",
+                link : "https://www.w3schools.com/python/numpy/numpy_quiz.asp"
+            },
+            {
+                title : "Pandas Quiz",
+                link : "https://www.w3schools.com/python/pandas/pandas_quiz.asp"
+            },
+            {
+                title : "SciPy Quiz",
+                link : "https://www.w3schools.com/python/scipy/scipy_quiz.php"
+            },
+            {
+                title : "TypeScript Quiz",
+                link : "https://www.w3schools.com/typescript/typescript_quiz.php"
+            },
+            {
+                title : "XML Quiz",
+                link : "https://www.w3schools.com/xml/xml_quiz.asp"
+            },
+            {
+                title : "R Quiz",
+                link : "https://www.w3schools.com/r/r_quiz.asp"
+            },
+            {
+                title : "Git Quiz",
+                link : "https://www.w3schools.com/git/git_quiz.asp"
+            },
+            {
+                title : "Kotlin Quiz",
+                link : "https://www.w3schools.com/kotlin/kotlin_quiz.php"
+            },
+            {
+                title : "Cyber Security Quiz",
+                link : "https://www.w3schools.com/quiztest/quiztest.php?qtest=CYBERSECURITY"
+            },
+            {
+                title : "Accessibility Quiz",
+                link : "https://www.w3schools.com/quiztest/quiztest.php?qtest=ACCESSIBILITY"
+            },
+        ]
+    },
+    Courses : {
+        name : "Courses",
+        menu : [
+            {
+                title : "HTML Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/html-course"
+            },
+            {
+                title : "CSS Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/css-course"
+            },
+            {
+                title : "JavaScript Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/javascript-course"
+            },
+            {
+                title : "Front End Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/front-end-course"
+            },
+            {
+                title : "Python Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/python-course"
+            },
+            {
+                title : "SQL Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/sql-course"
+            },
+            {
+                title : "PHP Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/php-course"
+            },
+            {
+                title : "Java Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/java-course"
+            },
+            {
+                title : "C++ Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/c-course-1"
+            },
+            {
+                title : "C# Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/c-course"
+            },
+            {
+                title : "jQuery Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/jquery-course"
+            },
+            {
+                title : "React.js Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/react-js-course"
+            },
+            {
+                title : "Bootstrap 4 Course",
+                link : "https://campus.w3schools.com/collections/single-courses/products/bootstrap-4-course"
+            },
+            {
+                title : "Bootstrap 3 Course",
+                link : "https://campus.w3schools.com/collections/single-courses/products/bootstrap-course"
+            },
+            {
+                title : "NumPy Course",
+                link : "https://campus.w3schools.com/products/numpy-course"
+            },
+            {
+                title : "Pandas Course",
+                link : "https://campus.w3schools.com/products/pandas-course"
+            },
+            {
+                title : "TypeScript Course",
+                link : "https://campus.w3schools.com/products/learn-typescript"
+            },
+            {
+                title : "XML Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/xml-course"
+            },
+            {
+                title : "R Course",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/r-course"
+            },
+            {
+                title : "Data Analytics Course",
+                link : "https://campus.w3schools.com/products/data-analytics-program"
+            },
+            {
+                title : "Cyber Security Course",
+                link : "https://campus.w3schools.com/products/cyber-security-course"
+            },
+            {
+                title : "Accessibility Course",
+                link : "https://campus.w3schools.com/products/accessibility-course"
+            },
+        ]
+    },
+    Certificates : {
+        name : "Certificates",
+        menu : [
+            {
+                title : "HTML Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/html-certificate"
+            },
+            {
+                title : "CSS Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/css-certificate"
+            },
+            {
+                title : "JavaScript Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/javascript-certificate"
+            },
+            {
+                title : "Front End Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/front-end-certificate"
+            },
+            {
+                title : "Python Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/python-certificate"
+            },
+            {
+                title : "SQL Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/sql-certificate"
+            },
+            {
+                title : "PHP Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/php-certificate"
+            },
+            {
+                title : "Java Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/java-certificate"
+            },
+            {
+                title : "C++ Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/c-certificate"
+            },
+            {
+                title : "C# Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/c-certificate-1"
+            },
+            {
+                title : "jQuery Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/jquery-certificate"
+            },
+            {
+                title : "React.js Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/react-js-certificate"
+            },
+            {
+                title : "MySQL Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/mysql-certificate"
+            },
+            {
+                title : "Bootstrap 5 Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/bootstrap-5-certificate"
+            },
+            {
+                title : "Bootstrap 4 Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/bootstrap-4-certificate"
+            },
+            {
+                title : "Bootstrap 3 Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/bootstrap-3-certificate"
+            },
+            {
+                title : "TypeScript Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/typescript-certificate"
+            },
+            {
+                title : "XML Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/xml-certificate"
+            },
+            {
+                title : "Excel Certificate",
+                link : "https://campus.w3schools.com/products/excel-certificate"
+            },
+            {
+                title : "Data Science Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/data-science-certificate"
+            },
+            {
+                title : "Cyber Security Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/cyber-security-certificate"
+            },
+            {
+                title : "Accessibility Certificate",
+                link : "https://campus.w3schools.com/collections/certifications/products/accessibility-certificate"
+            },
+        ]
+    },
+}
